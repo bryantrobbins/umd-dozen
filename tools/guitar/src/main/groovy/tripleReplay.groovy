eee33ff93b808573fa64dc40643d635197514067
Bryan Robbins <bryantrobbins@gmail.com>
2015-09-12 12:55:04 -0400

    Add waiting and results post job

--- tools/guitar/src/main/groovy/tripleReplay.groovy
+++ tools/guitar/src/main/groovy/tripleReplay.groovy
@@ -24,7 +24,7 @@ jobParams.put("DB_ID", args[1])
 jobParams.put("SUITE_ID", args[2])
 jobParams.put("BUNDLE_ID", bundleIds[0])
         
-// Use Jenkins client to launch jobs (we want 3 runs with same params)
+// Use Jenkins client to launch jobs (we want 3 runs with same params, except for bundle id)
 jenkinsClient.submitJob("replay-suite", jobParams)
 sleep(500)
 jobParams.put("BUNDLE_ID", bundleIds[1])
@@ -34,9 +34,56 @@ jobParams.put("BUNDLE_ID", bundleIds[2])
 jenkinsClient.submitJob("replay-suite", jobParams)
 sleep(500)
 
+// Wait for replay-suite jobs to be complete
+waitForBuildsComplete(client, "replay-suite")
+
+// Wait for individual replay jobs to be complete
+waitForBuildsComplete(client, "replay-test")
+
 // Now for the results job
 def resultsParams = [:]
 resultsParams.put("DB_ID", args[1])
 resultsParams.put("SUITE_ID", args[2])
 resultsParams.put("BUNDLE_IDS", "${bundleIds[0]},${bundleIds[1]},${bundleIds[2]}")
 jenkinsClient.submitJob("post-results", resultsParams)
+
+// Wait for results job to be complete
+waitForBuildsComplete(client, "post-results")
+
+void waitForBuildsComplete(RESTClient client, String buildName) {
+  // Wait for no jobs to be waiting
+  int waiting = getAwaitingBuildCount(client, buildName)
+  while(waiting > 0 ) {
+    println "Still ${waiting} ${buildName} jobs waiting to start"
+    Thread.sleep(300000)
+    waiting = getAwaitingBuildCount(client, buildName)
+  }
+
+  // Wait for no jobs to be in progress
+  int inprog = getInProgressBuildCount(client, buildName)
+  while(inprog > 0 ) {
+    println "Still ${inprog} ${buildName} jobs in progress"
+    Thread.sleep(300000)
+    waiting = getInProgressBuildCount(client, buildName)
+  }
+}
+
+int getAwaitingBuildCount(RESTClient client, String buildName){
+  def resp = client.get( path : '/queue/api/json' )
+  return resp.data['items'].findAll {
+    it.name.equals(buildName)
+  }.size()
+}
+
+int getInProgressBuildCount(RESTClient client, String buildName){
+  def resp = client.get( path : "/job/${buildName}/api/json")
+  int count = 0
+  for (def build : resp.data['builds']){
+    def respBuild = client.get( path : "/job/${buildName}/${build.id}/api/json")
+    if(respBuild.data['building'].equals("true")){
+      count++
+    }
+  }
+
+  return count
+}
