044ff32eb700525de38de97d6fc5c780da9811f3
Bryan Robbins <bryantrobbins@gmail.com>
2015-09-27 20:22:56 -0400

    Update svaeWalkingOutputs

--- tools/guitar/src/main/groovy/saveWalkingOutputs.groovy
+++ tools/guitar/src/main/groovy/saveWalkingOutputs.groovy
@@ -1,10 +1,9 @@
 // ARGS: HOST, PORT, DB_ID, EXEC_ID, COVERAGE_DIR
 
 import edu.umd.cs.guitar.main.TestDataManager
-import edu.umd.cs.guitar.processors.guitar.CoverageProcessor
 import edu.umd.cs.guitar.processors.guitar.LogProcessor
-import net.sourceforge.cobertura.coveragedata.ProjectData
-import edu.umd.cs.guitar.util.CoberturaUtils
+import edu.umd.cs.guitar.processors.guitar.MapProcessor
+import edu.umd.cs.guitar.processors.guitar.GUIProcessor
 import edu.umd.cs.guitar.artifacts.ArtifactCategory
 import groovy.io.FileType
 
@@ -14,41 +13,32 @@ host=args[0]
 port=args[1]
 dbId=args[2]
 execId=args[3]
-coverageDir=args[4]
 
 // Create a TestDataManager
 def manager = new TestDataManager(host, port, dbId)
 
-// Prepare args
-ArtifactCategory cat = ArtifactCategory.TEST_OUTPUT
-CoverageProcessor cp = new CoverageProcessor(manager.getDb())
+// Prepare processors
+ArtifactCategory cat = ArtifactCategory.TEST_INPUT
 LogProcessor lp = new LogProcessor()
+MapProcessor mp = new MapProcessor()
+GUIProcessor gp = new GUIProcessor()
 
-// Get list of coverage files from coverageDir
-fileList = []
-indexList = []
-def logFile = null
-
-def dir = new File(coverageDir)
-dir.eachFileRecurse (FileType.FILES) { file ->
-  def splits = file.getName().toString().split("\\.")
-  if(splits[-1].equals("ser") && (! (splits[0].charAt(0) == 't'))){
-    indexList << file.getName().toString().split("\\.")[0]
-    fileList << file.getAbsolutePath()
-  }
-	if(splits[-1].equals("log")){
-        logFilePath = file.getAbsolutePath()
-	}
-}
-
-//println fileList
-
-// Save each coverage artifact
-//fileList.eachWithIndex(){ path, i ->
-//	def opts = [(CoverageProcessor.FILE_PATH_OPTION): path, (CoverageProcessor.INDEX_OPTION): indexList[i] ]
-//	manager.saveArtifact(cat, cp, opts, execId)
-//}
-
-// Save log artifact
-def opts = [(LogProcessor.FILE_PATH_OPTION): logFilePath]
+// Save Log
+def opts = [(LogProcessor.FILE_PATH_OPTION): findLogFile() ]
 manager.saveArtifact(cat, lp, opts, execId)
+
+// Save Map
+def opts = [(MapProcessor.FILE_PATH_OPTION): "WalkingRipper.MAP"]
+manager.saveArtifact(cat, mp, opts, execId)
+
+// Save GUI
+def opts = [(GUIProcessor.FILE_PATH_OPTION): "WalkingRipper.GUI"]
+manager.saveArtifact(cat, gp, opts, execId)
+
+String findLogFile(){
+  def found = []
+  new File('.').eachFileMatch(FileType.ANY, ~/.*\.log/) {
+    found << it.name
+  }
+  found[0]
+}
