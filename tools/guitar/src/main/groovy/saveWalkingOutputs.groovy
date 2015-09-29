7c6bc0a942f3fd0ce7de48431af50879a34f85d0
Bryan Robbins <bryantrobbins@gmail.com>
2015-09-29 09:26:54 -0400

    Fix constructor args for MapProcessor

--- tools/guitar/src/main/groovy/saveWalkingOutputs.groovy
+++ tools/guitar/src/main/groovy/saveWalkingOutputs.groovy
@@ -3,7 +3,7 @@
 import edu.umd.cs.guitar.main.TestDataManager
 import edu.umd.cs.guitar.processors.guitar.LogProcessor
 import edu.umd.cs.guitar.processors.guitar.MapProcessor
-import edu.umd.cs.guitar.processors.guitar.GUIProcessor
+import edu.umd.cs.guitar.processors.guitar.TestcaseProcessor
 import edu.umd.cs.guitar.artifacts.ArtifactCategory
 import groovy.io.FileType
 
@@ -20,20 +20,23 @@ def manager = new TestDataManager(host, port, dbId)
 // Prepare processors
 ArtifactCategory cat = ArtifactCategory.TEST_INPUT
 LogProcessor lp = new LogProcessor()
-MapProcessor mp = new MapProcessor()
-GUIProcessor gp = new GUIProcessor()
+MapProcessor mp = new MapProcessor(manager.getDb())
+TestcaseProcessor tp = new TestcaseProcessor()
 
 // Save Log
+println "Saving log ${findLogFile()}"
 def opts = [(LogProcessor.FILE_PATH_OPTION): findLogFile() ]
 manager.saveArtifact(cat, lp, opts, testId)
 
 // Save Map
+println "Saving WalkingRipper.MAP"
 opts = [(MapProcessor.FILE_PATH_OPTION): "WalkingRipper.MAP"]
 manager.saveArtifact(cat, mp, opts, testId)
 
-// Save GUI
-opts = [(GUIProcessor.FILE_PATH_OPTION): "WalkingRipper.GUI"]
-manager.saveArtifact(cat, gp, opts, testId)
+// Save Testcase
+println "Saving WalkingRipper.TST"
+opts = [(TestcaseProcessor.FILE_PATH_OPTION): "WalkingRipper.TST"]
+manager.saveArtifact(cat, tp, opts, testId)
 
 String findLogFile(){
   def found = []
